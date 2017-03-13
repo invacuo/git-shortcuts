3a8870c98bc96cfbf38812ee537e5856695a3c3c
Chaitanya Kale <chaitanya.r.kale@gmail.com>
2017-03-13 15:14:09 -0400

    Updated to work with zsh

--- shortcuts.sh
+++ shortcuts.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+
+# change git checkout to git co
+git config --global alias.co checkout
+git config --global user.email "chaitanya.r.kale@gmail.com"
+git config --global user.name "Chaitanya Kale"
+
+
 #aliases for git commands
 alias gs='git status'
 alias gb='git branch'
@@ -17,7 +24,8 @@ function delbranch {
     return 1;
   fi
   while true; do
-    read -p "Are you sure you want to delete branch: $1?  : "  yn
+    echo "Are you sure you want to delete branch $1? (y/n): "
+    read yn
     case $yn in
       [Yy]* ) git push origin --delete $1;git branch -D $1;break;;
       [Nn]* ) break;;
@@ -35,9 +43,10 @@ function mergeit {
   while true; do
     git log origin/master..
     git diff --stat origin/master
-    read -p "Are you sure you want to merge this branch to master?" yn
+    echo "Are you sure you want to merge this branch to master? (y/n)"
+    read yn
     case $yn in
-      [Yy]* )   git push origin $1 && git checkout master && git merge $1 --ff-only && git push && delbranch $1;break;;
+      [Yy]* ) git push origin $1 && git checkout master && git merge $1 --ff-only && git push && delbranch $1;break;;
       [Nn]* ) break;;
       * ) echo "Please answer yes or no.";;
     esac
